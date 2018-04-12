d8bd9a68288ca32554e1859b23744a07d9c61738
Livia <liviaishak1@gmail.com>
2018-04-12 14:50:51 -0700

    cp2_init: completed

---
+++ lib/kele.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+
+
+class Kele
+    include HTTParty
+
+  def initialize(email, password)
+    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email": email, "password": password })
+    raise 'Invalid Email and/or Password' if response.code != 200
+    @auth_token = response["auth_token"]
+  end
+
+end
