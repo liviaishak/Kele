1e1c5d3a4c2bdb74fbd8f9e213bdd0cea8debe17
Livia <liviaishak1@gmail.com>
2018-04-14 21:44:46 -0700

    cp3: revised urls

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,21 +1,34 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap'
+
 
 class Kele
 
-    include HTTParty
+  include HTTParty
+  include RoadMap
+
+  base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    response = self.class.post('https://www.bloc.io/api/v1/sessions',
-      body: { "email": email, "password": password })
+    response = self.class.post(api_url("sessions"), body: { "email": email, "password": password })
     raise 'Invalid Email and/or Password' if response.code != 200
     @auth_token = response["auth_token"]
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me', 
-      headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
+  end
+
+  def get_mentor_available(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+  end
+
+  private
+  def api_url(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
   end
 
 end
