0e51d43df8a9bd1a9466818d7ad02f1ad6376138
Livia <liviaishak1@gmail.com>
2018-04-16 21:22:38 -0700

    cp6_messagin

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,26 +5,41 @@ require './lib/roadmap'
 
 class Kele
 
-    include HTTParty
-    include RoadMap
+  include HTTParty
+  include RoadMap
+
+  base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    response = self.class.post('https://www.bloc.io/api/v1/sessions',
-      body: { "email": email, "password": password })
+    response = self.class.post(api_url("sessions"), body: { "email": email, "password": password })
     raise 'Invalid Email and/or Password' if response.code != 200
     @auth_token = response["auth_token"]
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me',
-      headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
   end
 
   def get_mentor_available(mentor_id)
-    response = self.class.get('https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability',
-      headers: {"authorization" => @auth_token })
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body)
+  end
+
+  def get_messages
+    response = self.class.get(api_url("message_threads"), headers: {"authorization" => @auth_token })
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(email, recipient_id, token, subject, body)
+    response = self.class.post(api_url("messages"), body: { "sender" => email, "recipient_id" => recipient_id, "token" => token, "subject" => subject, "striped-text" => message }, headers: { "authorization" => @auth_token })
+    puts reponse
+  end
+
+
+  private
+  def api_url(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
   end
 
 end
